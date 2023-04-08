28541077c401f33228a2363537a73b0f7c718b5f
¨sktbrd¨ <sktbrd.eth@gmail.com>
2023-04-08 17:32:08 -0300

    fix Home opening posts twice

--- src/components/PostModal.vue
+++ src/components/PostModal.vue
@@ -1,20 +1,26 @@
 <template>
+  <!-- Q-dialog component to display the post modal -->
   <q-dialog v-model="showPost" position="top">
+    <!-- Q-card component to display the content of the modal -->
     <q-card class="q-pa-md" style="max-width: 800px;">
+      <!-- Q-card-section component to contain the post content -->
       <q-card-section>
+        <!-- Q-item-label component to display the post author -->
         <q-item-label>
           {{ post.author }}
         </q-item-label>
+        <!-- div element to display the post body -->
         <div
               v-if="post.body"
               class="text-body2 q-mt-sm"
               v-html="marked(post.body)"
               style="word-wrap: break-word;"
             />
-
       </q-card-section>
+      <!-- Q-card-actions component to contain the modal actions -->
       <q-card-actions align="right">
-        <q-btn label="Close" color="primary" v-close-popup />
+        <!-- Q-btn component to close the modal -->
+        <q-btn label="Close" color="primary" @click="handleClose" v-close-popup />
       </q-card-actions>
     </q-card>
   </q-dialog>
@@ -26,34 +32,45 @@ import { marked } from 'marked'
 export default {
   name: 'Post-modal',
   props: {
+    // Boolean prop to control the display of the modal
     modelValue: {
       type: Boolean,
       required: true
     },
+    // Object prop containing the post data to display
     post: {
       type: Object,
       required: true
     }
   },
   data () {
+    // Data property to store the current state of the modal
     return {
       showPost: this.modelValue
     }
   },
   watch: {
+    // Watcher to update the modal state when the prop changes
     modelValue (value) {
       this.showPost = value
     }
   },
   computed: {
+    // Computed property to return the marked function from the marked library
     marked () {
       return marked
     },
+    // Computed property to parse and return the hashtags from the post body
     hashtagArray () {
       return this.hashtags
         .split(',')
         .map((tag) => tag.trim())
         .filter((tag) => tag !== '')
+    }
+  },
+  methods: {
+    handleClose () {
+      console.log('Close button clicked')
     }
   }
 }
--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,15 +1,21 @@
 <template>
+  <!-- Load the custom CSS stylesheet for the component -->
   <head>
-  <link rel="stylesheet" href="src/css/app.css">
-</head>
+    <link rel="stylesheet" href="src/css/app.css">
+  </head>
 
+  <!-- Q-page component with padding -->
   <q-page padding>
+    <!-- Display a welcome message to the user if logged in -->
     <div v-if="user">
       <h3> Hello {{ username }}, welcome to the revolution</h3>
     </div>
-    <PostModal ref="postModal" :post="selectedPost" :modelValue="showPostModal" />
+    <!-- Component to display the post modal, with props passed from the parent component -->
+    <PostModal ref="postModal" :post="selectedPost" v-model="showPostModal" />
+    <!-- Display the posts using the v-for directive to iterate through the posts array -->
     <div class="q-gutter-md q-matrix">
       <div v-for="(post, index) in posts" :key="index" class="q-col-xs-12 q-col-md-4 q-mb-md">
+        <!-- Q-card component with an on-click event to show the post modal, and displaying the post information -->
         <q-card @click="showPost(post)" class="cursor-pointer">
           <div style="height: 150px; overflow: hidden;">
             <img :src="'https://images.ecency.com/u/hive-173115/avatar/large'" style="width: 100%; height: 100%; object-fit: cover;" />
@@ -21,7 +27,6 @@
         </q-card>
       </div>
     </div>
-    <PostModal ref="postModal" :post="selectedPost" v-model="showPostModal" />
   </q-page>
 </template>
 
@@ -35,6 +40,7 @@ export default {
     PostModal
   },
   data () {
+    // Data properties to store the posts, search query, user, selected post, and post modal state
     return {
       posts: [],
       search: '',
@@ -44,14 +50,17 @@ export default {
     }
   },
   mounted () {
+    // Call the getPosts() method when the component is mounted
     this.getPosts()
   },
   computed: {
+    // Computed property to return the username of the logged-in user
     username () {
       return this.user ? this.user.name : ''
     }
   },
   methods: {
+    // Asynchronous method to fetch posts using the dhive client
     async getPosts () {
       const { dhive } = getDhive()
       const dhiveClient = new dhive.Client(['https://api.hive.blog', 'https://api.hivekings.com', 'https://anyx.io', 'https://api.openhive.network'])
@@ -62,6 +71,7 @@ export default {
       const posts = await dhiveClient.database.getDiscussions('created', query)
       this.posts = posts
     },
+    // Method to show the post modal and set the selected post
     showPost (post) {
       this.selectedPost = post
       this.showPostModal = true
